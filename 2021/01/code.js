d11962a9d24500f9c3c57dcb3d670c33f9ab142a
Hamilton Evans <glenn.h.evans@gmail.com>
2021-12-01 08:14:01 -0500

    Day 2 Part 2 - Remembering Lodash

--- 2021/01/code.js
+++ 2021/01/code.js
@@ -10,8 +10,15 @@ function part1(input) {
 }
 
 function part2(input) {
-    return "tbd";
+    let increased = 0;
+    let prevWindow = _.sum(_.slice(input, 0, 3));
+    for (let i = 4; i <= input.length; i++) {
+        let window = _.sum(_.slice(input, i-3, i));
+        increased += (window > prevWindow) ? 1 : 0;
+        prevWindow = window;
+    }
+    return increased;
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 2 - " + part2(input));
