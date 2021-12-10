24f37c8b4512aca9c876e0d07bbe1fc9cdd1e24c
Hamilton Evans <glenn.h.evans@gmail.com>
2021-12-10 07:25:02 -0500

    Day 9 Part 1 - Too easy, part 2 will probably suck

--- 2021/09/code.js
+++ 2021/09/code.js
@@ -1,8 +1,26 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
 
-function part1(input) {
-    return "tbd";
+function part1(map) {
+    // console.log(map)
+
+    let totalRisk = 0;
+    for (let y = 0; y < map.length; y++) {
+        for (let x = 0; x < map[0].length; x++) {
+            let height =  map[y][x];
+            let up = map[y-1]?.[x] ?? 10;
+            let down = map[y+1]?.[x] ?? 10;
+            let left = map[y]?.[x-1] ?? 10;
+            let right = map[y]?.[x+1] ?? 10;
+
+            if (height < up && height < down && height < left && height < right) {
+                // console.log(`\t${up}\n${left}\t${map[y][x]}\t${right}\n\t${down}`)
+                // console.log(`LOW POINT FOUND\n`)
+                totalRisk += height+1;
+            }
+        }
+    }
+    return totalRisk;
 }
 
 function part2(input) {
@@ -10,4 +28,4 @@ function part2(input) {
 }
 
 console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(input));
+// console.log("Part 2 - " + part2(input));
