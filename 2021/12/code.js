69e86cef213a305f6cd21a270db2747742529151
Hamilton Evans <glenn.h.evans@gmail.com>
2021-12-13 14:08:47 -0500

    Day 12 Part 2 - Doubling back, and reading instructions properly

--- 2021/12/code.js
+++ 2021/12/code.js
@@ -4,8 +4,7 @@ const {input, testInput} = require('./input');
 let paths = [];
 function followPath(caves, currentCave, path) {
     if (currentCave === 'end') {
-        path += 'end';
-        paths.push(path);
+        paths.push(path+'end');
         return;
     }
 
@@ -19,30 +18,40 @@ function followPath(caves, currentCave, path) {
 
 function part1(caves) {
     followPath(caves, 'start', '');
-    console.log(paths);
+    // console.log(paths);
     return paths.length;
 }
 
 function followPath2(caves, currentCave, path) {
     if (currentCave === 'end') {
-        path += 'end';
-        paths.push(path);
+        paths.push(path+'end');
         return;
     }
-
     path += `${currentCave},`;
+
+    let canDoubleBack = true;
+    let smallCaves = path.split(',').filter(cave => cave.toLowerCase() === cave);
+    for (cave of smallCaves) {
+        if(path.split(',').filter(c => c === cave).length === 2) {
+            canDoubleBack = false;
+        }
+    }
+
     for(cave of caves.get(currentCave)) {
-        if (cave.toUpperCase() === cave || (path.split(',').indexOf(cave) === -1)) {
-            followPath(caves, cave, path);
+        if (cave.toUpperCase() === cave || path.split(',').indexOf(cave) === -1) { // big or new cave
+            followPath2(caves, cave, path);
+        } else if (_.filter(path.split(','), c => c === cave).length < 2 && canDoubleBack) { // can go twice
+            followPath2(caves, cave, path);
         }
     }
 }
 
 function part2(caves) {
-    followPath(caves, 'start', '');
-    console.log(paths);
+    followPath2(caves, 'start', '');
+    // console.log(paths);
     return paths.length;
 }
 
-// console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(testInput));
+console.log("Part 1 - " + part1(input));
+paths = [];
+console.log("Part 2 - " + part2(input));
