0cb231a84709b180741fd247e681d3db01834567
Hamilton Evans <glenn.h.evans@gmail.com>
2021-12-17 08:54:13 -0500

    Day 17 Parts 1&2 - Shooting probes, some brute forcing required

--- 2021/17/code.js
+++ 2021/17/code.js
@@ -1,13 +1,65 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
 
-function part1(input) {
-    return "tbd";
+function runSim(target) {
+
+    let bestOverall = {
+        maxHeight: 0
+    };
+    let totalHits = 0;
+
+    for (let y = -1000; y < 1000; y++) {
+        for (let x = 0; x <= target.maxX; x++) {
+            let probe = {
+                initX: x,
+                initY: y,
+                x: 0,
+                y: 0,
+                vx: x,
+                vy: y,
+                maxHeight: 0
+            };
+        
+            while(true) {
+                probe = moveProbe(probe);
+                if (outOfRange(probe, target)) {
+                    // console.log(`out of bounds for: [${probe.x},${probe.y}] with vel [${probe.vx},${probe.vy}]`)
+                    break;
+                } else if (hitTarget(probe, target)) {
+                    // console.log(`HIT TARGET for: [${probe.x},${probe.y}] with maxHeight ${probe.maxHeight}`)
+                    bestOverall = (probe.maxHeight > bestOverall.maxHeight) ? probe : bestOverall;
+                    totalHits++;
+                    break;
+                }
+            }
+        }
+    }
+
+    return { bestOverall, totalHits};
 }
 
-function part2(input) {
-    return "tbd";
+function moveProbe(probe) {
+    return {
+        initX: probe.initX,
+        initY: probe.initY,
+        x: probe.x + probe.vx,
+        y: probe.y + probe.vy,
+        vx: probe.vx + ((probe.vx > 0) ? -1 : (probe.vx < 0) ? 1 : 0),
+        vy: probe.vy - 1,
+        maxHeight: (probe.y > probe.maxHeight) ? probe.y : probe.maxHeight
+    }
 }
 
-console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(input));
+function outOfRange(probe, target) {
+    return (probe.y < target.minY) || (probe.x > target.maxX); 
+}
+
+function hitTarget(probe, target) {
+    return ((probe.x >= target.minX && probe.x <= target.maxX) && 
+            (probe.y >= target.minY && probe.y <= target.maxY));
+}
+
+let {bestOverall, totalHits} = runSim(input);
+
+console.log("Part 1 - " + bestOverall.maxHeight);
+console.log("Part 2 - " + totalHits);
--- 2021/17/input.js
+++ 2021/17/input.js
@@ -1,4 +1,15 @@
-let input = ``;
-let testInput = ``;
+let input = {
+    minX: 88,
+    maxX: 125, 
+    minY: -157,
+    maxY: -103
+};
+
+let testInput = {
+    minX: 20,
+    maxX: 30, 
+    minY: -10,
+    maxY: -5
+};
 
 module.exports = {input, testInput};
