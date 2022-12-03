fe6f267987fab933bbdacfe9ffd7060e8e500b46
Hamilton Evans <glenn.h.evans@gmail.com>
2022-12-03 09:13:06 -0500

    Day 3 Part 2 - Once again over thought part 1

--- 2022/03/code.js
+++ 2022/03/code.js
@@ -24,16 +24,30 @@ function part1(list) {
     return getPriorities(sacks.map(sack => { return sack.shared}));
 }
 
-
 function getPriorities(items) {
     return items.map(item => {
                 return (item === item.toUpperCase()) ? item.charCodeAt() - 38 : item.charCodeAt() - 96;
             }).reduce((a,b) => a+b, 0);
 }
 
-function part2(input) {
-    return "tbd";
+function part2(list) {
+    let sacks = list.split('\n');
+    let groups = [];
+    while (sacks.length > 0) {
+        groups.push(sacks.splice(0, 3));
+    }
+
+    let vals = groups.map(group => {
+        let first = group[0];
+        for (let i = 0; i < first.length; i++) {
+            if (group[1].indexOf(first[i]) > -1 && group[2].indexOf(first[i]) > -1) {
+                return first[i];
+            }
+        }
+    })
+    
+    return getPriorities(vals);
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 2 - " + part2(input));
