6e6c499e15e8902ea5e782ddd10cb4f688a829e5
Hamilton Evans <glenn.h.evans@gmail.com>
2022-12-12 09:12:34 -0500

    Day 12 Part 2 - Used dijkstra package and massaged data into proper structure, part two was easy

--- 2022/12/code.js
+++ 2022/12/code.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
+const dijkstra = require('dijkstrajs');
 
 const getHeight = function(char) {
     return 'abcdefghijklmnopqrstuvwxyz'.indexOf(char);
@@ -13,14 +14,17 @@ const canMove = function(cur, adjacent) {
     return (curVal >= adjacentVal) || (curVal + 1 === adjacentVal);
 }
 
-class Node {
-    constructor(val, l, r, u, d) {
-        this.val = val,
-        this.l = l,
-        this.r = r,
-        this.u = u,
-        this.d = d
+const getAdjacent = function(map, y, x, visited) {
+    let adj = [];
+    let dy = [0,-1,0,1] // l,u,r,d
+    let dx = [-1,0,1,0] // l,u,r,d
+    for(let i = 0; i < 4; i++) {
+        let next = map[y+dy[i]]?.[x+dx[i]];
+        if(next !== undefined && canMove(map[y][x], next) && visited[`${y+dy[i]},${x+dx[i]}`] !== true) {
+            adj.push([[y+dy[i], x+dx[i]],1])
+        }
     }
+    return adj;
 }
 
 function part1(input) {
@@ -29,55 +33,45 @@ function part1(input) {
     })
 
     let startPos, exitPos;    
+    let graph = {}
     for (let y = 0; y < droneMap.length; y++) {
         for (let x = 0; x < droneMap[0].length; x++) {
             if (droneMap[y][x] === 'S') { startPos = {x: x, y: y}}
             if (droneMap[y][x] === 'E') { exitPos = {x: x, y: y}}
+
+            graph[`${y},${x}`] = Object.fromEntries(getAdjacent(droneMap, y, x, []));
         }
     }
-
-    dfs(droneMap, startPos.x, startPos.y, exitPos.x, exitPos.y);
-    return "tbd";
-}
-
-function getAdjacent(map, y, x, visited) {
-    let adj = [];
-    let dy = [0,-1,0,1] // l,u,r,d
-    let dx = [-1,0,1,0] // l,u,r,d
-    for(let i = 0; i < 4; i++) {
-        let next = map[y+dy[i]]?.[x+dx[i]];
-        if(next !== undefined && canMove(map[y][x], next) && visited[`${y+dy[i]},${x+dx[i]}`] !== true) {
-            adj.push([y+dy[i], x+dx[i]])
-        }
-    }
-    return adj;
-}
-
-function dfs(map, startX, startY, exitX, exitY) {
-    let numSteps = 0;
-    let visited = {};
-    let stack = [];
-    stack.push([startY, startX]);
-
-    while(stack.length > 0) {
-        let loc = stack.pop();
-        if (!visited[`${loc[0]},${loc[1]}`]) {
-            numSteps++;
-            console.log(`visiting: ${loc}`)
-            if (`${loc[0]},${loc[1]}` === `${exitY},${exitX}`) {
-                console.log(`FOUND THE EXIT AFTER ${numSteps}`)
-                break;
-            }
-            visited[`${loc[0]},${loc[1]}`] = true;
-            stack.push(...getAdjacent(map, loc[0], loc[1], visited));
-        }
-    }
-    return numSteps;
+    
+    let path = dijkstra.find_path(graph, `${startPos.y},${startPos.x}`, `${exitPos.y},${exitPos.x}`)
+    return path.length - 1;
 }
 
 function part2(input) {
-    return "tbd";
+    let droneMap = input.split('\n').map(row => {
+        return row.split('');
+    })
+
+    let startPos = [], exitPos;    
+    let graph = {};
+    for (let y = 0; y < droneMap.length; y++) {
+        for (let x = 0; x < droneMap[0].length; x++) {
+            if (droneMap[y][x] === 'S' || droneMap[y][x] === 'a') { startPos.push({x: x, y: y})}
+            if (droneMap[y][x] === 'E') { exitPos = {x: x, y: y}}
+
+            graph[`${y},${x}`] = Object.fromEntries(getAdjacent(droneMap, y, x, []));
+        }
+    }
+    
+    let shortest = Infinity;
+    for (start of startPos) {
+        try {
+            let path = dijkstra.find_path(graph, `${start.y},${start.x}`, `${exitPos.y},${exitPos.x}`);
+            shortest = (path.length - 1 < shortest) ? path.length - 1 : shortest;
+        } catch (e) { }
+    }
+    return shortest;
 }
 
-console.log("Part 1 - " + part1(testInput));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(input));
