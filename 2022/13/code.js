67f1c123c5dbd294c8e148142305883cf6643af6
Hamilton Evans <glenn.h.evans@gmail.com>
2022-12-13 12:16:21 -0500

    This is so painful

--- 2022/13/code.js
+++ 2022/13/code.js
@@ -5,8 +5,8 @@ const getPairs = function(input) {
     return input.split('\n\n').map(pair => {
         let parts = pair.split('\n');
         return {
-            p1: parts[0],//.slice(1,parts[0].length-1),
-            p2: parts[1]//.slice(1,parts[1].length-1)
+            p1: JSON.parse(parts[0]),//.slice(1,parts[0].length-1),
+            p2: JSON.parse(parts[1])//.slice(1,parts[1].length-1)
         }
     });
 }
@@ -49,86 +49,87 @@ const parsePacket = function(packet) {
     return parts;
 }
 
-const checkLists = function(p1, p2) {
-    for (let i = 0; i < p1.length; i++) {
-        let left = p1[i], right = p2[i];
-        console.log(`  - Compare ${left} vs ${right}`) 
-        if (left !== right) {
-            if (left > right) {
-                console.log(`    - Right side is smaller, so inputs are NOT in the right order`);
-                return false;
-            } else {
-                console.log(`    - Left side is smaller, so inputs are in the right order`);
+function comparePackets(packet1, packet2) {
+    let max = Math.max(packet1.length, packet2.length)
+    for (let i = 0; i < max; i++) {
+        let left = (Array.isArray(packet1[i])) ? packet1[i] : [packet1[i]];
+        let right = (Array.isArray(packet2[i])) ? packet2[i] : [packet2[i]];
+        let retVal = doComparison(left, right);
+        if (retVal !== undefined) {
+            if (retVal) {
+                console.log(`    - Left side is smaller, so inputs are in the right order`)
                 return true;
+            } else if (!retVal) {
+                console.log(`    - Right side is smaller, so inputs are NOT in the right order`)
+                return false
             }
         }
     }
+    console.log(`    - These packets are identical`);
     return true;
 }
 
-const isNumber = function(val) {
-    console.log(val)
-    if (['[',']',','].includes(val)) {
-        return false;
-    } else {
-        return true;
+function doComparison(leftArray, rightArray) {        
+    console.log(`  - Compare ${JSON.stringify(leftArray)} vs ${JSON.stringify(rightArray)}`);
+
+    let max = Math.max(leftArray.length, rightArray.length)
+    for(let i = 0; i < max; i++) {
+        let leftNested = _.countBy(JSON.stringify(leftArray))['['] > 1;
+        let rightNested = _.countBy(JSON.stringify(rightArray))['['] > 1;
+        // console.log(`leftNested: ${leftNested}`)
+        // console.log(`rightNested: ${rightNested}`)
+        let left = leftArray?.[i];
+        let right = rightArray?.[i];
+
+        if (left?.length > 1 && right?.length === undefined) {
+            return doComparison(left, rightArray)
+        } else if(right?.length > 1 && left?.length === undefined) {
+            return doComparison(leftArray, right);
+        } else if (left?.length > 1 && right?.length > 1) {
+            return doComparison(left, right);
+        } else if (left === undefined && right !== undefined) {
+            console.log(`  - Left side ran out of items, so inputs are in the right order`)
+            return true;
+        } else if (left !== undefined && right === undefined) {
+            console.log(`  - Right side ran out of items, so inputs are NOT in the right order`)
+            return false;
+        } else {
+            console.log(`    - Compare ${JSON.stringify(left)} vs ${JSON.stringify(right)}`);
+            if (left !== right) {
+                return (left < right) ? true : false;
+            }
+        }
     }
 }
 
 function part1(input) {
     let pairs = getPairs(input);
-    console.log(pairs)
     let goodIndexes = []
     let badIndexes = [];
     let idx = 1;
     for (pair of pairs) {
-        console.log(pair);
-        console.log(JSON.parse(pair.p1))
-        console.log(JSON.parse(pair.p2))
-        let p1Parts = parsePacket(pair.p1);
-        let p2Parts = parsePacket(pair.p2);
-        console.log(p1Parts)
-        console.log(p2Parts)
-
         console.log(`== Pair ${idx} ==`)
-        console.log(`- Compare [${p1Parts}] vs. [${p2Parts}]`)
-        let max = Math.max(p1Parts.length, p2Parts.length)
-        for(let i = 0; i < max; i++) {
-            // let left = p1Parts[i];
-            // console.log(`left: ${left}`);
-            // right = p2Parts[i];
-            // console.log(`right: ${right}`)
-            // if (left[0] === '[') {
-            //     left = left.slice(1, left.length -1).split(',')
-            //     console.log(`updated left`)
-            //     console.log(left)
-            // }
+        console.log(`- Compare ${JSON.stringify(pair.p1)} vs. ${JSON.stringify(pair.p2)}`)
 
-            let left = (p1Parts[i]?.[0] === '[') ? p1Parts[i].slice(1,p1Parts[i].length-1) : p1Parts[i];
-            let right = (p2Parts[i]?.[0] === '[') ? p2Parts[i].slice(1,p2Parts[i].length-1) : p2Parts[i];
-            
-            console.log(`  - Compare ${left} vs ${right}`) 
-            console.log(isNumber(left))
-            console.log(isNumber(right))
-            // console.log(left)
-            // console.log(right)
-            // if (Number.isInteger(left) && Number.isInteger(right)) {
-            //     console.log(`pure int comparision: ${left} vs. ${right}`)
-            // } else {
-            //     console.log(`mixed comparison: ${left} vs. ${right}`)
-            // }
+        if(comparePackets(pair.p1, pair.p2)) {
+            goodIndexes.push(idx);
+        } else {
+            badIndexes.push(idx);
         }
-        // console.log(checkLists(p1Parts, p2Parts))
-        
         console.log()
+
+        if (idx > 16) {
+            return
+        }
         idx++;
     }
-    return "tbd";
+    
+    return goodIndexes.reduce((a,b) => a+b, 0);
 }
 
 function part2(input) {
     return "tbd";
 }
 
-console.log("Part 1 - " + part1(testInput));
+console.log("Part 1 - " + part1(input));
 // console.log("Part 2 - " + part2(input));
