53978f297d97ff5b98e333ce65dd66902016cdf3
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-01 08:47:04 -0500

    Day 1 P1 - knocking off the rust

--- 2023/01/code.js
+++ 2023/01/code.js
@@ -1,8 +1,12 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
 
-function part1(input) {
-    return "tbd";
+function part1(calibrationDoc) {
+    let numbers = calibrationDoc.split('\n')
+                                .map(l => l.split('').filter(Number))
+                                .map(a => Number(a[0] + a[a.length -1]));
+    
+    return numbers.reduce((a,b) => a+b, 0);
 }
 
 function part2(input) {
@@ -10,4 +14,4 @@ function part2(input) {
 }
 
 console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(input));
+// console.log("Part 2 - " + part2(input));
