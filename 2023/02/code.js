2e34b4bfc87f75d7205c12e7f91406aeb8d1cb56
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-02 09:33:21 -0500

    Cleaned up a bit

--- 2023/02/code.js
+++ 2023/02/code.js
@@ -1,34 +1,17 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
 
-const RED = 12, GREEN = 13, BLUE = 14;
-
 function part1(input) {
     let sumOfValid = 0;
     
     input.forEach(game => {
         let valid = true;
-        game.draws.forEach(draw => {
-            draw.forEach(set => {
-                switch(set.color) {
-                    case 'red':
-                        if (set.num > RED) {
-                            valid = false;
-                        }
-                        break;
-                    case 'green':
-                        if (set.num > GREEN) {
-                            valid = false;
-                        }
-                        break;
-                    case 'blue':
-                        if (set.num > BLUE) {
-                            valid = false;
-                        }
-                        break;
-                }
-            })
-        })
+        game.draws.map(draw => draw.map(set => {
+            let maxVal = (set.color ==='red') ? 12 : (set.color === 'green') ? 13 : 14;
+            if (set.num > maxVal) {
+                valid = false;
+            }
+        }));
         
         // console.log(`Game: ${game.id} => ${valid}`);
         if (valid) { sumOfValid+= game.id; }
@@ -41,34 +24,32 @@ function part2(input) {
     
     input.forEach(game => {
         let minR = minG = minB = 0;
-        game.draws.forEach(draw => {
-            draw.forEach(set => {
-                switch(set.color) {
-                    case 'red':
-                        if (set.num > minR ) {
-                            minR = set.num;
-                        }
-                        break;
-                    case 'green':
-                        if (set.num > minG ) {
-                            minG = set.num;
-                        }
-                        break;
-                    case 'blue':
-                        if (set.num > minB ) {
-                            minB = set.num;
-                        }
-                        break;
-                }
-            })
-        })
-        let power = minR * minG * minB;
-        console.log(`Game: ${game.id} => power = ${power}`);
-        
-        sumPower += power;
+        game.draws.map(draw => draw.map(set => {
+            switch(set.color) {
+                case 'red':
+                    if (set.num > minR ) {
+                        minR = set.num;
+                    }
+                    break;
+                case 'green':
+                    if (set.num > minG ) {
+                        minG = set.num;
+                    }
+                    break;
+                case 'blue':
+                    if (set.num > minB ) {
+                        minB = set.num;
+                    }
+                    break;
+            }
+        }));
+
+        // console.log(`Game: ${game.id} => power = ${minR * minG * minB}`);
+        sumPower += (minR * minG * minB);
     })
+
     return sumPower;
 }
 
-// console.log("Part 1 - " + part1(input));
+console.log("Part 1 - " + part1(input));
 console.log("Part 2 - " + part2(input));
