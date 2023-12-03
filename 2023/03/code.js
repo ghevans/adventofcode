55196568ad83155ab5b9207b1c7cbbf82138ff86
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-03 10:56:45 -0500

    day 3 pt 2 - after parsing it wasn't so bad, did a bunch of clean up though

--- 2023/03/code.js
+++ 2023/03/code.js
@@ -13,22 +13,29 @@ function part1(input) {
 }
 
 function checkNear(num, symbols) {
-    symbols = symbols.filter(s => (Math.abs(s.y - num.y) <= 1));
-    for (symbol of symbols) {
-        let minX = num.x - 1;
-        let maxX = num.x + (''+num.val).length;
-
-        if (symbol.x <= maxX && symbol.x >= minX) {
-            return true;
-        }
-    }
-
-    return false;
+    symbols = symbols.filter(s => (Math.abs(s.y - num.y) <= 1))
+                     .filter(s => ((s.x <= num.x + (''+num.val).length) && (s.x >= num.x - 1)));
+    
+    return (symbols.length > 0) ? true : false;
 }
 
 function part2(input) {
-    return "tbd";
+    let nums = input.filter(i => _.isNumber(i.val))
+    let gears = input.filter(i => i.val === '*')
+
+    let sum = 0;
+    for (gear of gears) {
+        sum += findGears(gear, nums);
+    }
+    return sum;
+}
+
+function findGears(gear, nums) {
+    nums = nums.filter(n => (Math.abs(n.y - gear.y) <= 1))
+               .filter(n => ((gear.x >= n.x - 1) && (gear.x <= n.x + (''+n.val).length)));
+
+    return (nums.length === 2) ? nums.reduce((a,b) => a.val*b.val) : 0;
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
