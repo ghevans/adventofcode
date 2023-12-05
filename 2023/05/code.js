b2aee24ad47ab0bfe383417c65df599fad58a151
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-05 08:05:11 -0500

    Day 5 pt 1 - a bit of a chonker

--- 2023/05/code.js
+++ 2023/05/code.js
@@ -1,15 +1,53 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
 
-console.log(input);
+const steps = [
+    'seed-to-soil',
+    'soil-to-fertilizer',
+    'fertilizer-to-water',
+    'water-to-light',
+    'light-to-temperature',
+    'temperature-to-humidity',
+    'humidity-to-location'
+]
+
 function part1(input) {
-    console.log(input)
-    return "tbd";
+    let locations = [];
+
+    for (seed of input.seeds) {
+        let current = seed;
+        let mapping = {};
+        for (step of steps) {
+            let lines = input[step];
+            let valid = false;
+            for (part of lines) {
+                let next = 0;
+                if ((current <= part.source + part.range) && current >= part.source) {
+                    valid = true;
+                    next = part.dest + (current - part.source);
+                }
+
+                if(valid) { 
+                    current = next;
+                    break; 
+                }
+            }
+            mapping[step] = current;
+        }
+        locations.push({
+            seed: seed,
+            loc: current,
+            steps: mapping
+        });
+    }
+    // console.log(locations)
+
+    return _.minBy(locations, 'loc').loc;
 }
 
 function part2(input) {
     return "tbd";
 }
 
-console.log("Part 1 - " + part1(testInput));
+console.log("Part 1 - " + part1(input));
 // console.log("Part 2 - " + part2(testInput));
