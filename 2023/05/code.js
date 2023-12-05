294b1e9c7adf47b7b075699ca281d9aa06bcf531
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-05 07:30:18 -0500

    Day 5 - Parsing

---
+++ 2023/05/code.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+const {input, testInput} = require('./input');
+
+console.log(input);
+function part1(input) {
+    console.log(input)
+    return "tbd";
+}
+
+function part2(input) {
+    return "tbd";
+}
+
+console.log("Part 1 - " + part1(testInput));
+// console.log("Part 2 - " + part2(testInput));
