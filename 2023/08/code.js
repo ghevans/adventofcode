c86e63ea4c43f3ce921b214d787f9dfa2b2099c1
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-08 10:28:18 -0500

    Day 8 Pt 2 - went in circles without rememberin where I was supposed to be...finally got back

--- 2023/08/code.js
+++ 2023/08/code.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
-const {input, testInput} = require('./input');
+const { lcm } = require('mathjs')
+const {input, testInput, testInput2} = require('./input');
 
 function part1(input) {
     let loc = 'AAA';
@@ -17,8 +18,26 @@ function part1(input) {
 }
 
 function part2(input) {
-    return "tbd";
+    let startingNodes = Object.keys(input.map).filter(node => node[2] === 'A');
+    let totalSteps = [];
+    
+    for (let i = 0; i < startingNodes.length; i++) {
+        let curNode = startingNodes[i];
+        let steps = 0;
+        while(true) {
+            let nextDir = input.inst[steps%input.inst.length];
+            let nextNode = (nextDir === 'L') ? input.map[curNode][0] : input.map[curNode][1];
+            steps++;
+
+            if(nextNode[2] === 'Z') {
+                break;
+            }
+            curNode = nextNode;
+        }
+        totalSteps.push(steps);
+    }
+    return lcm(...totalSteps);
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
