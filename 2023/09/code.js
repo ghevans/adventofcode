f7c9754e97591bc3f31e58f7b475e03d87bd0ce3
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-09 08:17:33 -0500

    Day 9 Pt 2 - fastest second star yet

--- 2023/09/code.js
+++ 2023/09/code.js
@@ -4,12 +4,12 @@ const {input, testInput} = require('./input');
 function part1(input) {
     let nextVals = [];
     for (line of input) {
-        nextVals.push(line[line.length-1] + findDiff(line));
+        nextVals.push(line[line.length-1] + findNext(line));
     }
     return _.sum(nextVals);
 }
 
-function findDiff(input) {
+function findNext(input) {
     let diff = [];
     let nextVal = 0;
     for (let i = 1; i < input.length; i++) {
@@ -17,15 +17,33 @@ function findDiff(input) {
     }
 
     if (diff.some(n => n !== 0)) {
-        nextVal = findDiff(diff);
+        nextVal = findNext(diff);
     }
 
     return nextVal + diff[diff.length-1];
 }
 
 function part2(input) {
-    return "tbd";
+    let nextVals = [];
+    for (line of input) {
+        nextVals.push(line[0] - findPrev(line));
+    }
+    return _.sum(nextVals);
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+function findPrev(input) {
+    let diff = [];
+    let nextVal = 0;
+    for (let i = 1; i < input.length; i++) {
+        diff.push(input[i] - input[i-1]);
+    }
+
+    if (diff.some(n => n !== 0)) {
+        nextVal = findPrev(diff);
+    }
+
+    return diff[0] - nextVal;
+}
+
+// console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(input));
