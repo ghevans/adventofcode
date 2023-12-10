e5ad053ebfba1e4163c2fcdf158c66bee0ae20f3
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-10 14:59:20 -0500

    Day 10 Pt 2 - turned the grid into proper blocks and then manually removed things not in the pipe...gross. BFS would be the proper solution

--- 2023/10/code.js
+++ 2023/10/code.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
-const {input, testInput} = require('./input');
+const {input, testInput, testInput2} = require('./input');
 
-function part1(input) {
+function buildLoop(input, dirs) {
     let startLoc = {};
     let map = Array.from(Array(input.length), () => new Array(input[0].length))
     for (let y = 0; y < input.length; y++) {
@@ -15,8 +15,6 @@ function part1(input) {
         }
     }
 
-    // let dirs = ['r','d']; <-- test data directions
-    let dirs = ['u','l'];
     let allPaths = Array.from(Array(dirs.length), () => new Map());
     for (let i = 0; i < dirs.length; i++) {
         let curDir = dirs[i]; // this just iterates through our possible starting loops
@@ -89,16 +87,94 @@ function part1(input) {
         }
     }
 
+    return allPaths;
+}
+
+function part1(input, dirs) {
+    let loops = buildLoop(input, dirs);
+    
     let minDists = {};
-    allPaths[0].forEach((value, key) => {
-        minDists[key] = Math.min(value, allPaths[1].get(key));
+    loops[0].forEach((value, key) => {
+        minDists[key] = Math.min(value, loops[1].get(key));
     });
     return _.max(Object.values(minDists));
 }
 
-function part2(input) {
-    return "tbd";
+function part2(input, dirs) {
+    let loop = buildLoop(input, dirs)[0];
+    
+    let yLimits = {};
+    for (let i = 0; i < input.length; i++) {
+        yLimits[i] = {
+            min: input[0].length,
+            max: 0
+        }
+    }
+    let xLimits = {};
+    for (let i = 0; i < input[0].length; i++) {
+        xLimits[i] = {
+            min: input.length,
+            max: 0
+        }
+    }
+    loop.forEach((value, key) => {
+        let [y, x] = key.split(',');
+        yLimits[y].min = (yLimits[y].min > x) ? Number(x) : yLimits[y].min;
+        yLimits[y].max = (yLimits[y].max < x) ? Number(x) : yLimits[y].max;
+        
+        xLimits[x].min = (xLimits[x].min > y) ? Number(y) : xLimits[x].min;
+        xLimits[x].max = (xLimits[x].max < y) ? Number(y) : xLimits[x].max;
+    })
+
+    let print = '*' + Array.from(Array(input[0].length), () => '*').join('') + '*\n';
+    let totalHoles = 0;
+    for (let y = 0; y < input.length; y++) {
+        let parts = input[y].split('');
+        let newLine = `* `;
+        for (let x = 0; x < input[0].length; x++) {
+            if (!loop.has(`${y},${x}`)) {
+                if ((x < yLimits[y].min || x > yLimits[y].max) ||
+                    (y < xLimits[x].min || y > xLimits[x].max)) {
+                        newLine += ' ';
+                } 
+                else {
+                    newLine += '#';
+                    totalHoles++;
+                }
+            } else {
+                let char = '';
+                switch(parts[x]) {
+                    case '|':
+                        char = '│';
+                        break;
+                    case '-': 
+                        char = '─';
+                        break;
+                    case 'L':
+                        char = '└';
+                        break;
+                    case 'J':
+                    case 'S':
+                        char = '┘';
+                        break;
+                    case '7':
+                        char = '┐';
+                        break;
+                    case 'F':
+                        char = '┌';
+                        break;            
+                }
+                newLine += char;
+            }
+        }
+        print += newLine + '*\n'
+    }
+    print += '*' + Array.from(Array(input[0].length), () => '*').join('') + '*\n';
+    console.log(print);
+
+    return totalHoles;
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 1 - " + part1(input, ['u','l']));
+console.log("Part 2 - " + part2(input, ['u','l']));
+// console.log("Part 2 - " + part2(testInput2, ['d','r']));
