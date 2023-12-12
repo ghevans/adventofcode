090fcf7b806385c74ac68204f7b58776e0217765
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-12 17:13:36 -0500

    I quit

--- 2023/12/code.js
+++ 2023/12/code.js
@@ -11,42 +11,66 @@ const buildRegex = (groups) => {
     }
     return RegExp(r.substring(0, r.length-5),'g')
 }
-const buildOptions = (spring, groups, options, regex) => {
-    if ((options.has(spring.join('')) ||
-        (optionsLeft(spring) < spotsNeeded(groups)))) {
-        return;
+const buildOptions = (spring, groups, options, valid, regex) => {
+    if (spring.every(c => c !== '?')) {
+        // if(options.has(spring.join(''))) {
+        //     return; 
+        // } else {
+            options.add(spring.join(''));
+        // }
+
     }
+    // options.add(spring.join(''))
+    // console.log(`checking: ${spring.join('')}`)
+    // if ((options.has(spring.join('')) ||
+    //     (optionsLeft(spring) < spotsNeeded(groups)))) {
+    //     return;
+    // }
+
+    // if (spring.join('') === '.#.###.#.######') {
+    //     console.log('here')
+    // }
 
     if (spring.every(c => c !== '?') && optionsLeft(spring) === spotsNeeded(groups)) {
         let match = regex.exec(spring.join(''));
-        if (match) {
-            options.add(spring.join(''));
+        if (match && !valid.has(spring.join(''))) {
+            console.log(spring.join(''))
+            valid.add(spring.join(''));
         }
     } else {
-        for(let i = 0; i < spring.length; i++) {
-            if (spring[i] === '?') {
-                let part = [...spring];
-                part.splice(i,1,'.');
-                buildOptions(part, groups, options, regex);
-                part.splice(i,1,'#');
-                buildOptions(part, groups, options, regex);
-            }
+        let flips = spring.map((c, i) => [c, i]).filter(([c]) => c === '?').map(([c, i]) => i);
+        for(i of flips) {
+        // for(let i = 0; i < spring.length; i++) {
+            // if (spring[i] === '?') {
+                let part1 = [...spring];
+                part1.splice(i,1,'.');
+                // if (!options.has(part.join(''))) {
+                    buildOptions(part1, groups, options, valid, regex);
+                // }
+                let part2 = [...spring];
+                part2.splice(i,1,'#');
+                // if (!options.has(part.join(''))) {
+                    buildOptions(part2, groups, options, valid, regex);
+                // }
+            // }
         }
     }
 }
 
 function part1(input) {
     let totalArrangements = 0;
-    for (spring of input) {
+    for (const [index, spring] of input.entries()) {
         let options = new Set();
-
+        let valid = new Set();
         let regex = buildRegex(spring.groups);
 
-        // console.log(`Starting to work on ${spring.spring}`)
-        buildOptions(spring.spring, spring.groups, options, regex);
-        console.log(`Finished working on ${spring.spring} which has [${options.size}] arrangements`)
-
-        totalArrangements += options.size;
+        console.log(`Starting to work on ${spring.spring} with groups ${spring.groups}`)
+        // console.log(regex)
+        // console.log(index)
+        buildOptions(spring.spring, spring.groups, options, valid, regex);
+        console.log(valid)
+        // console.log(options.has('.#.###.#.######'))
+        totalArrangements += valid.size;
         // break;
     }
     return totalArrangements;
