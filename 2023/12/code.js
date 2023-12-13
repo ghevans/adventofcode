cd780d50a1a8329700e9c9e3e7705924d05e2800
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-13 15:52:34 -0500

    Day 12 pt 2 - rebuilt it entirely and it worked!

--- 2023/12/code.js
+++ 2023/12/code.js
@@ -11,7 +11,8 @@ const buildRegex = (groups) => {
     }
     return RegExp(r.substring(0, r.length-6) + '[^#]*')
 }
-const getArrangements = (spring, groups, options, valid, regex) => {
+
+const getArrangements = (spring, groups, valid, regex) => {
     if (spring.every(c => c !== '?') && optionsLeft(spring) === spotsNeeded(groups)) {
         let match = regex.exec(spring.join(''));
         if (match) {
@@ -22,47 +23,19 @@ const getArrangements = (spring, groups, options, valid, regex) => {
         if (flips.length) {
             let part1 = [...spring];
             part1.splice(flips[0], 1, '.');
-            getArrangements(part1, groups, options, valid, regex);
+            getArrangements(part1, groups, valid, regex);
             
             let part2 = [...spring];
             part2.splice(flips[0], 1, '#');
-            getArrangements(part2, groups, options, valid, regex);
+            getArrangements(part2, groups, valid, regex);
         }
     }
+
+    return valid.size;
 }
 
-function part1(input) {
-    let totalArrangements = 0;
-    for (const [index, spring] of input.entries()) {
-        let options = new Set();
-        let valid = new Set();
-        
-        getArrangements(spring.spring, spring.groups, options, valid, buildRegex(spring.groups));
-        
-        totalArrangements += valid.size;
-    }
-    return totalArrangements;
-}
-
-function part2(input) {
-    let expandedRecords = unfold(input);
-
-    let totalArrangements = 0;
-    for (spring of expandedRecords) {
-        let options = new Set();
-        let valid = new Set();
-        console.log(`Starting ${spring.spring}`)
-        getArrangements(spring.spring, spring.groups, options, valid, buildRegex(spring.groups));
-        console.log(`Found ${valid.size} arrangements`)
-        totalArrangements += valid.size;
-    }
-    return totalArrangements;
-}
-
-// ???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
-// ???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
 const numFolds = 5;
-function unfold(input) {
+const unfold = (input) => {
     let out = [];
     for (spring of input) {
         spring.spring.push('?')
@@ -77,5 +50,83 @@ function unfold(input) {
     return out;
 }
 
-// console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(testInput));
+class Possibility {
+    constructor(curGroup, curCount, arrangements) {
+        this.curGroup = curGroup,
+        this.curCount = curCount;
+        this.arrangements = arrangements;
+    }
+}
+
+function workSmarter(springs, groups) {
+    let options = [new Possibility(0,0,1)];
+    for (let i = 0; i < springs.length; i++) {
+        let currentSpring = springs[i];
+
+        if (currentSpring === '?') {
+            // walk through all current options and potentionally add the new options / prune if needed
+            options = options.flatMap(opt => {
+                let out = [];
+                if(opt.curGroup < groups.length && opt.curCount < groups[opt.curGroup]) { // # case as long as we don't go past the count we need
+                    out.push(new Possibility(opt.curGroup, opt.curCount+1, opt.arrangements));
+                }
+
+                if (opt.curCount === 0) { // . case, if count=0, we weren't building toward a group, so pass it along
+                    out.push(opt);
+                } else if (opt.curCount === groups[opt.curGroup]) { // . case, this checks if we closed out a group with this .
+                    out.push(new Possibility(opt.curGroup+1, 0, opt.arrangements));
+                }
+                return out;
+            }).filter(opt => opt !== undefined); // remove any steps that were pruned
+        } else {
+            // walk through all current options and adjust based on # or . (3 options: we finished a group (.), we have a non-impactful (.), we add to a currently building group as long as it's not too big / too many groups (#))
+            options = options.flatMap(opt => {
+                if (currentSpring === '#' && opt.curGroup < groups.length && opt.curCount < groups[opt.curGroup]) { // this will add to a current group as long as we haven't passed the needed number AND we aren't building more groups than needed
+                    return new Possibility(opt.curGroup, opt.curCount+1, opt.arrangements);
+                } else if (currentSpring === '.' && opt.curCount === 0) { // if count = 0, we weren't building toward a group, so pass this along
+                    return opt;
+                } else if (currentSpring === '.' && opt.curCount === groups[opt.curGroup]) { // we properly closed/spaced a group
+                    return new Possibility(opt.curGroup+1, 0, opt.arrangements);
+                }
+            }).filter(opt => opt !== undefined); // remove any steps that were pruned
+        }
+        
+        // Remove options that can't work
+        options = options.filter(opt => opt.curGroup <= groups.length)
+                        .filter(opt => (springs.length - i + opt.curCount) >= groups.slice(opt.curGroup).reduce((a,b) => a+b, 0));
+
+
+        // There should be a better way of this, but basically count up duplicates for curGroup/curCount and collapse them back down to a single state
+        let collapsed = new Map();
+        for (opt of options) {
+            if (collapsed.has(`${opt.curGroup},${opt.curCount}`)) {
+                collapsed.set(`${opt.curGroup},${opt.curCount}`, collapsed.get(`${opt.curGroup},${opt.curCount}`) + opt.arrangements);
+            } else {
+                collapsed.set(`${opt.curGroup},${opt.curCount}`, opt.arrangements);
+            }
+        }
+
+        // Start the next loop with no dups
+        options = [];
+        for (const [key, value] of collapsed.entries()) {
+            options.push(new Possibility(Number(key.split(',')[0]), Number(key.split(',')[1]), value))
+        }
+    }
+
+    // Final clean up to remove anything that didn't finish properly
+    options = options.filter(opt => (opt.curGroup === groups.length) || ((opt.curGroup === (groups.length - 1)) && opt.curCount === groups[opt.curGroup]));
+
+    return options.map(opt => opt.arrangements).reduce((a,b) => a+b,0);
+}
+
+
+function part1(input) {
+    return input.map(spring => getArrangements(spring.spring, spring.groups, new Set(), buildRegex(spring.groups))).reduce((a,b) => a+b,0);
+}
+
+function part2(input) {
+    return unfold(input).map(spring => workSmarter(spring.spring, spring.groups)).reduce((a,b) => a+b,0);
+}
+
+console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(input));
