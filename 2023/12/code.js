51e05f0dec25132f6e5bd4f7558e2592082eb96b
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-13 06:54:20 -0500

    Probably the end of trying pt 2

--- 2023/12/code.js
+++ 2023/12/code.js
@@ -45,8 +45,37 @@ function part1(input) {
 }
 
 function part2(input) {
-    return "tbd";
+    let expandedRecords = unfold(input);
+
+    let totalArrangements = 0;
+    for (spring of expandedRecords) {
+        let options = new Set();
+        let valid = new Set();
+        console.log(`Starting ${spring.spring}`)
+        getArrangements(spring.spring, spring.groups, options, valid, buildRegex(spring.groups));
+        console.log(`Found ${valid.size} arrangements`)
+        totalArrangements += valid.size;
+    }
+    return totalArrangements;
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+// ???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
+// ???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3
+const numFolds = 5;
+function unfold(input) {
+    let out = [];
+    for (spring of input) {
+        spring.spring.push('?')
+        let s = Array(numFolds).fill(spring.spring).flat();
+        s.pop();
+        let g = Array(numFolds).fill(spring.groups);
+        out.push({
+            spring : s.flat(),
+            groups: g.flat()
+        })
+    }
+    return out;
+}
+
+// console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(testInput));
