b651381ca29d36a9d9a8c2b7ddcd47e1f6704e1a
Hamilton Evans <glenn.h.evans@gmail.com>
2023-12-15 08:26:41 -0500

    Day 15 Pt 2 - Took forever to understand the question, the coding wasn't bad

--- 2023/15/code.js
+++ 2023/15/code.js
@@ -8,16 +8,74 @@ function part1(sequence) {
 function getHash(part) {
     let hash = 0;
     for (char of part) {
-        hash += char.charCodeAt(0);
-        hash *= 17
-        hash = (hash % 256);
+        if (['-', '='].includes(char)) {
+            break;
+        } else {
+            hash += char.charCodeAt(0);
+            hash *= 17
+            hash = (hash % 256);
+        }
     }
     return hash;
 }
 
-function part2(input) {
-    return "tbd";
+
+const findLabel = (label, box) => {
+    for (let i = 0; i < box.length; i++) {
+        if (box[i].label === label) {
+            return i;
+        }
+    }
+    return -1;
 }
 
+function part2(sequence) {
+    let boxes = Array.from(Array(256), () => new Array())
+    const instructions = sequence.map(part => {
+        let locOfSymbol = (part.indexOf('=') > -1) ? part.indexOf('=') : part.indexOf('-');
+        return {
+            label : part.slice(0, locOfSymbol).join(''),
+            box : getHash(part),
+            oper : (part.indexOf('=') > -1) ? '=' : '-',
+            focalLength : (part.indexOf('=') > -1) ? Number(part[locOfSymbol+1]) : -1
+        }
+    });
+
+    for (inst of instructions) {
+        let curBox = boxes[inst.box];
+        // console.log(`operation ${inst.oper} on box ${inst.box} with label ${inst.label}`)
+        switch(inst.oper) {
+            case '=':
+                if(curBox.length === 0) {
+                    curBox.push(inst);
+                } else {
+                    let labelIndex = findLabel(inst.label, curBox);
+                    if (labelIndex > -1) {
+                        curBox.splice(labelIndex, 1, inst);
+                    } else {
+                        curBox.push(inst);
+                    }
+                }
+                break;
+            case '-':
+                let labelIndex = findLabel(inst.label, curBox);
+                if(labelIndex > -1) {
+                    curBox.splice(labelIndex, 1);
+                }
+                break;
+        }
+    }
+    boxes = boxes.map((box, index) => [index, box]).filter(([index, box]) => box.length > 0);
+    let power = 0;
+    for ([boxNum, boxList] of boxes) {
+        for (let i = 0; i < boxList.length; i++) {
+            power += (1+boxNum) * (i+1) * boxList[i].focalLength;
+        }
+    }
+    
+    return power;
+}
+
+
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
