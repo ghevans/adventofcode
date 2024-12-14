c52392a929a8079038b091de48de496163a6f068
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-14 18:25:40 -0500

    Day 2 Part 2 - Coming back to me slowly, but it's happening

--- 2024/02/code.js
+++ 2024/02/code.js
@@ -3,49 +3,70 @@ const {input, testInput} = require('./input');
 const helper = require('../../helpers')
 
 function part1(input) {
-    return checkSafe(input, false).length;
+    return checkSafe(input)[0].length;
 }
 
 function part2(input) {
-    return checkSafe(input, true).length;
+    let [safe, unsafe] = checkSafe(input);
+    return safe.length + checkDampenerSafe(unsafe).length;
 }
 
-const checkSafe = (reports, dampener) => {
-    let safe = [];
-    for (report of reports) {
-        let isSafe = true;
-        let dampenerUsed = false;
-        let decreasing = report[0] > report[1];
-        for (let i = 1; i < report.length; i++) {
-            let diff = report[i-1] - report[i];
-            
-            if (diff === 0) {
-                if (dampener && !dampenerUsed) {
-                    dampenerUsed = true;
-                }
-                isSafe = false;
-                break; 
-            } 
-            if (Math.abs(diff) > 3) {
+const checkReport = (report) => {
+    let isSafe = true;
+    let decreasing = report[0] > report[1];
+    for (let i = 1; i < report.length; i++) {
+        let diff = report[i-1] - report[i];
+        
+        if (diff === 0) {
+            isSafe = false;
+            break; 
+        } 
+        if (Math.abs(diff) > 3) {
+            isSafe = false;
+            break;
+        }
+        if (decreasing) {
+            if (diff < 0) {
                 isSafe = false;
                 break;
             }
-            if (decreasing) {
-                if (diff < 0) {
-                    isSafe = false;
-                    break;
-                }
-            } else {
-                if (diff > 0) {
-                    isSafe = false;
-                    break;
-                }
+        } else {
+            if (diff > 0) {
+                isSafe = false;
+                break;
             }
         }
-        if (isSafe) { safe.push(report); }
+    }
+    return isSafe;
+}
+
+const checkSafe = (reports) => {
+    let safe = [], unsafe = [];
+    for (report of reports) {
+        if (checkReport(report)) {
+            safe.push(report);
+        } else {
+            unsafe.push(report);
+        }
+    }
+    return [safe, unsafe];
+}
+
+const checkDampenerSafe = (reports) => {
+    let safe = [];
+    for (report of reports) {
+        for (let i = 0; i < report.length; i++) {
+            let tempReport = _.clone(report);
+            tempReport.splice(i,1);
+            if (checkReport(tempReport)) {
+                safe.push(report);
+                break;
+            }
+        }
+
     }
     return safe;
 }
 
-// console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(testInput));
+console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(input));
