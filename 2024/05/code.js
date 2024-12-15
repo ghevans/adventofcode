da7bf11194b1966b991b9e2ec33fc8d319069b1c
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-15 13:14:19 -0500

    Day 5 Part 2 - This was the first tricky day, mostly because I had to work out some more advanced structures for the data, and then understand how to iteratively correct

--- 2024/05/code.js
+++ 2024/05/code.js
@@ -1,44 +1,90 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
-const helper = require('../../helpers')
+const helper = require('../../helpers');
+
+const checkUpdates = (rules, updates) => {
+    let validUpdates = [], invalidUpdates = [];
+
+    for (update of updates) {
+        if (checkUpdate(rules, update)) { 
+            validUpdates.push(update); 
+        } else {
+            invalidUpdates.push(update);
+        }
+    }
+
+    return [validUpdates, invalidUpdates];
+}
+
+const checkUpdate = (rules, update) => {
+    for(let i = 0; i < update.length; i++) {
+        let rule = rules.get(update[i]);
+        let prevPages = update.slice(0, i), postPages = update.slice(i+1);
+        
+        for (prev of prevPages) {
+            if (!rule.after.includes(prev)) {
+                return false;
+            }
+        }
+        for (post of postPages) {
+            if (!rule.before.includes(post)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 
 function part1(input) {
-    const rules = input[0];
-    const updates = input[1];
-
-    let validUpdates = [];
-    for (update of updates) {
-        
-        let valid = true;
-        for(let i = 0; i < update.length; i++) {
-            let rule = rules.get(update[i]);
-            let prevPages = update.slice(0, i), postPages = update.slice(i+1);
-            
-            for (prev of prevPages) {
-                if (!rule.after.includes(prev)) {
-                    valid = false;
-                    break;
-                }
-            }
-            for (post of postPages) {
-                if (!rule.before.includes(post)) {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (!valid) { break; }
-        }
-
-        if (valid) { validUpdates.push(update); }
-    }
+    [validUpdates, invalidUpdates] = checkUpdates(input[0], input[1]);
 
     return validUpdates.map(u => u[Math.round((u.length - 1) / 2)]).reduce((a,b) => a + b);
 }
 
+const fixOnce = (rules, update) => {
+    for(let i = 0; i < update.length; i++) {
+        let rule = rules.get(update[i]);
+        let prevPages = update.slice(0, i), postPages = update.slice(i+1);
+        
+        for (prev of prevPages) {
+            if (!rule.after.includes(prev)) {
+                [update[update.indexOf(prev)], update[update.indexOf(update[i])]] = [update[update.indexOf(update[i])], update[update.indexOf(prev)]];
+                return update;
+            }
+        }
+        for (post of postPages) {
+            if (!rule.before.includes(post)) {
+                [update[update.indexOf(post)], update[update.indexOf(update[i])]] = [update[update.indexOf(update[i])], update[update.indexOf(post)]];
+                return update;
+            }
+        }
+    }
+
+    return true;
+}
+
+const correctInvalid = (rules, updates) => {
+    let correctedUpdates = [];
+
+    for (option of updates) {
+        while (true) {
+            option = fixOnce(rules, option);
+            if (checkUpdate(rules, option)) {
+                correctedUpdates.push(option);
+                break;
+            }
+        }
+    }
+
+    return correctedUpdates;
+}
+
 function part2(input) {
-    return "tbd";
+    [validUpdates, invalidUpdates] = checkUpdates(input[0], input[1]);
+    
+    return correctInvalid(input[0], invalidUpdates).map(u => u[Math.round((u.length - 1) / 2)]).reduce((a,b) => a + b, 0);
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
