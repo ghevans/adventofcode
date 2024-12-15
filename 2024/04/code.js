3ea49227a2420674622a244609bea7619a64256a
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-15 09:36:53 -0500

    Day 4 Part 1 - Diagonals completed

--- 2024/04/code.js
+++ 2024/04/code.js
@@ -7,12 +7,21 @@ const right = (row, x) => { return (x + 3 > row.length) ? false : row.slice(x, x
 const up = (map, x, y) => { return left(map.map(r => r[x]), y); }
 const down = (map, x, y) => { return right(map.map(r => r[x]), y); }
 
+const upLeft = (map, x, y) => { return ((x >= 3) && ( y >= 3)) ? map[y][x] + map[y-1][x-1] + map[y-2][x-2] + map[y-3][x-3] === 'XMAS' : false; }
+const upRight = (map, x, y) => { return ((x <= map[0].length - 3) && ( y >= 3)) ? map[y][x] + map[y-1][x+1] + map[y-2][x+2] + map[y-3][x+3] === 'XMAS' : false; }
+const downLeft = (map, x, y) => { return ((x >= 3) && (y < map.length - 3)) ? map[y][x] + map[y+1][x-1] + map[y+2][x-2] + map[y+3][x-3] === 'XMAS' : false; }
+const downRight = (map, x, y) => { return ((x <= map[0].length - 3) && (y < map.length - 3)) ? map[y][x] + map[y+1][x+1] + map[y+2][x+2] + map[y+3][x+3] === 'XMAS' : false; }
+
 function part1(input) {
     console.log(left(input[4],6));
     console.log(right(input[0],5));
     console.log(up(input, 9, 9));
     console.log(down(input, 9, 3));
-
+    console.log(upLeft(input, 6, 5));
+    console.log(upLeft(input, 9, 9));
+    console.log(upRight(input, 0, 5))
+    console.log(downLeft(input, 9, 3))
+    console.log(downRight(input, 4, 0))
     return "tbd";
 }
 
