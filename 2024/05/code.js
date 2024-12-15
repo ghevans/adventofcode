0aba3a1220879237bca678ef44509c6211732a1a
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-15 11:34:34 -0500

    Day 5 Part 1 - Parsing complete

---
+++ 2024/05/code.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+const {input, testInput} = require('./input');
+const helper = require('../../helpers')
+
+function part1(input) {
+    console.log(input)
+    return "tbd";
+}
+
+function part2(input) {
+    return "tbd";
+}
+
+console.log("Part 1 - " + part1(testInput));
+// console.log("Part 2 - " + part2(testInput));
