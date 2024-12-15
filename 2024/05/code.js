8376a2d105caed28b17c6e4265580663a069f698
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-15 12:14:51 -0500

    Day 5 Part 1 - Parsing this one was fun

--- 2024/05/code.js
+++ 2024/05/code.js
@@ -3,13 +3,50 @@ const {input, testInput} = require('./input');
 const helper = require('../../helpers')
 
 function part1(input) {
-    console.log(input)
-    return "tbd";
+    const rules = input[0];
+    const updates = input[1];
+
+    let validUpdates = [];
+    for (update of updates) {
+        // console.log(`\nChecking UPDATE: ${update}`);
+        let valid = true;
+        for(let i = 0; i < update.length; i++) {
+            let rule = rules.get(update[i]);
+            let prevPages = update.slice(0, i), postPages = update.slice(i+1);
+            // console.log(`\tChecking page rule ${update[i]}, must be before ${rule.before} and after ${rule.after}`);
+            // console.log(`\t\tagainst these prevPages - ${prevPages} and these postPages - ${postPages}`)
+            
+            for (prev of prevPages) {
+                if (!rule.after.includes(prev)) {
+                    valid = false;
+                    break;
+                }
+            }
+            for (post of postPages) {
+                if (!rule.before.includes(post)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (!valid) { break; }
+        }
+
+        if (valid) {
+            // console.log(`\tUPDATE is valid`)
+            validUpdates.push(update);
+        } else {
+            // console.log(`\tUPDATE is invalid`)
+        }
+    }
+
+    // console.log(validUpdates)
+    return validUpdates.map(u => u[Math.round((u.length - 1) / 2)]).reduce((a,b) => a + b);
 }
 
 function part2(input) {
     return "tbd";
 }
 
-console.log("Part 1 - " + part1(testInput));
+console.log("Part 1 - " + part1(input));
 // console.log("Part 2 - " + part2(testInput));
