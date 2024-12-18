a5f7d16ff2d88cb97a5673a666c864385d17cb76
Hamilton Evans <glenn.h.evans@gmail.com>
2024-12-18 06:52:10 -0500

    Day 7 Part 2 - Seemed trickier than it turned out to be

--- 2024/07/code.js
+++ 2024/07/code.js
@@ -1,25 +1,10 @@
 const _ = require('lodash');
 const {input, testInput} = require('./input');
-const helper = require('../../helpers')
+const helper = require('../../helpers');
 
-function part1(input) {
-    let correct = [];
-
-    for (line of input) {
-        if (performStep(line.output, line.vals[0], _.clone(line.vals.slice(1)), '+', `${line.vals[0]}`)) {
-            correct.push(line);
-        } else if(performStep(line.output, line.vals[0], _.clone(line.vals.slice(1)), '*', `${line.vals[0]}`)) {
-            correct.push(line);
-        }
-    }
-
-    // console.log(correct);
-    return correct.map(v => v.output).reduce((a,b) => a + b);
-}
-
-const performStep = (output, currentVal, rest, operator, str) => {
+const performStep = (output, currentVal, rest, operator, str, allowConcat) => {
     let nextOper = rest.shift();
-    let nextVal = (operator === '+') ? currentVal + nextOper : currentVal * nextOper;
+    let nextVal = (operator === '+') ? currentVal + nextOper : (operator === '*') ? currentVal * nextOper : Number(''+ currentVal + nextOper);
 
     if (nextVal > output) { 
         return false;
@@ -27,17 +12,36 @@ const performStep = (output, currentVal, rest, operator, str) => {
         return (output === nextVal);
     }
 
-    if (performStep(output, nextVal, _.clone(rest), '+', str.concat(` ${operator} ${nextOper}`))) {
+    if (performStep(output, nextVal, _.clone(rest), '+', str.concat(` ${operator} ${nextOper}`), allowConcat)) {
         return true;
     }
-    if (performStep(output, nextVal, _.clone(rest), '*', str.concat(` ${operator} ${nextOper}`))) {
+    if (performStep(output, nextVal, _.clone(rest), '*', str.concat(` ${operator} ${nextOper}`), allowConcat)) {
         return true;
+    }
+    if (allowConcat) {
+        if (performStep(output, nextVal, _.clone(rest), '||', str.concat(` ${operator} ${nextOper}`), allowConcat)) {
+            return true;
+        }
     }
 }
 
-function part2(input) {
-    return "tbd";
+function part1(input, allowConcat) {
+    let correct = [];
+
+    for (line of input) {
+        if (performStep(line.output, line.vals[0], _.clone(line.vals.slice(1)), '+', `${line.vals[0]}`, allowConcat)) {
+            correct.push(line);
+        } else if(performStep(line.output, line.vals[0], _.clone(line.vals.slice(1)), '*', `${line.vals[0]}`, allowConcat)) {
+            correct.push(line);
+        } else if(allowConcat) {
+            if (performStep(line.output, line.vals[0], _.clone(line.vals.slice(1)), '||', `${line.vals[0]}`, allowConcat)) {
+                correct.push(line);
+            }
+        }
+    }
+
+    return correct.map(v => v.output).reduce((a,b) => a + b);
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 1 - " + part1(input, false));
+console.log("Part 2 - " + part1(input, true));
