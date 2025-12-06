3bec71a381d74c69e5dbd6f71acde4a49bce3475
Hamilton Evans <glenn.h.evans@gmail.com>
2025-12-06 11:53:52 -0500

    Day 1 Pt 2 - got a bit wonky for day 1

--- 2025/01/code.js
+++ 2025/01/code.js
@@ -3,47 +3,50 @@ const {input, testInput} = require('./input');
 const helper = require('../../helpers')
 
 function part1(input) {
-    let pos = 50, max = 99, min = 0;
-    let runs = 0;
-    console.log(`The dial starts by pointing at 50`)
-    for (ins of input) {
-        // console.log(`turning ${ins.dir} for ${ins.val} turns`);
-        let fullTurns = Math.trunc(ins.val / 100);
-        let actMoves = ins.val % 100;
-        let from = pos;
-        switch (ins.dir) {
-            case 'L':
-                nextPos = pos - actMoves;
-                if (nextPos < 0) {
-                    pos = 100 + nextPos;
-                } else {
-                    pos = nextPos;
-                }
-                break;
-            case 'R':
-                nextPos = pos + actMoves;
-                if (nextPos > 99) {
-                    pos = nextPos - 100;
-                } else {
-                    pos = nextPos;
-                }
-                break;
-        }
-    
-        if (ins.dir === 'R') {
-            console.log(`[${ins.dir}${ins.val}]:\t[${from} -> ${pos}] which was ${fullTurns} full turns and (${actMoves}) moves`)
-        } else {
-            console.log(`[${ins.dir}${ins.val}]:\t[${pos} <- ${from}] which was ${fullTurns} full turns and (${actMoves}) moves`)
-        }
-        runs++;
-        // if (runs >= 100) break;
-    }
-    return pos;
+    return rotate(input, false);
 }
 
 function part2(input) {
-    return "tbd";
+    return rotate(input, true);
+}
+
+function rotate(input, countAll) {
+    let pos = 50, numZeros = 0;
+    for (ins of input) {
+        if (countAll) { 
+            numZeros += Math.trunc(ins.val / 100); // we start knowing we've done N full turns before we dial in
+        }
+        let moves = ins.val % 100, touchedZero = 0;
+
+        if (moves > 0) { // if we are doing a pure rotation, don't move anything
+            switch (ins.dir) {
+                case 'L':
+                    nextPos = pos - moves;
+                    if (nextPos < 0) { // we crossed zero again
+                        touchedZero = (pos !== 0) ? 1 : 0; // if we start at 0, ignore
+                        pos = 100 + nextPos;
+                    } else {
+                        touchedZero += (nextPos === 0) ? 1 : 0; // check if we landed on zero
+                        pos = nextPos;
+                    }
+                    break;
+                case 'R':
+                    nextPos = pos + moves;
+                    if (nextPos > 99) { // we crossed zero again
+                        touchedZero = (pos !== 0) ? 1 : 0; // if we start at 0, ignore
+                        pos = nextPos - 100;
+                    } else {
+                        touchedZero += (nextPos === 0) ? 1 : 0; // check if we landed on zero
+                        pos = nextPos;
+                    }
+                    break;
+            }
+        }
+
+        numZeros += (countAll) ? touchedZero : (pos === 0) ? 1 : 0; // Count the touches
+    }
+    return numZeros;
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
