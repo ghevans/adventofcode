bd299a4689d1a2e83c23de878329091443942712
Hamilton Evans <glenn.h.evans@gmail.com>
2025-12-07 13:07:44 -0500

    Day 4 Part 2 - This was fun

--- 2025/04/code.js
+++ 2025/04/code.js
@@ -7,17 +7,41 @@ function part1(input) {
     for(y=0; y < input.length; y++) {
         for(x = 0; x < input[0].length; x++) {
             if (input[y][x] === '@') {
-                count += (helper.getAllNeighbors(input, y, x).filter(c => c==='@').length < 4) ? 1 : 0
+                count += (helper.getAllNeighbors(input, y, x).filter(c => c==='@').length < 4) ? 1 : 0;
             }
         }
     }
     return count;
 }
 
-
 function part2(input) {
-    return "tbd";
+    let count = 0, toRemove = [], updated = true;
+
+    while (updated) {
+        updated = false;
+        for(y=0; y < input.length; y++) {
+            for(x = 0; x < input[0].length; x++) {
+                if (input[y][x] === '@') {
+                    if (helper.getAllNeighbors(input, y, x).filter(c => c==='@').length < 4) {
+                        count++;
+                        toRemove.push({y: y, x: x});
+                        updated = true;
+                    }
+                }
+            }
+        }
+        input = removeRolls(input, toRemove);
+    }
+    return count;
+}
+
+function removeRolls(grid, toRemove) {
+    let out = grid;
+    for (loc of toRemove) {
+        out[loc.y][loc.x] = '.'
+    }
+    return out;
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(testInput));
+console.log("Part 2 - " + part2(input));
