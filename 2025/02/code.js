09639c831c686bc7a29a1d48e0eac0cdabaf2718
Hamilton Evans <glenn.h.evans@gmail.com>
2025-12-07 11:09:38 -0500

    cleanup

--- 2025/02/code.js
+++ 2025/02/code.js
@@ -6,7 +6,8 @@ function part1(input) {
     let invalidIds = new Set();
     for ([start, end] of input) {
         for (let i = start; i <= end; i++) {
-            let patternNum = buildNumber(getPattern(i));
+            let patternNum = Number(getPattern(i).repeat(2));
+
             if (!invalidIds.has(patternNum)) {
                 if (patternNum >= start && patternNum <= end) {
                     invalidIds.add(patternNum)
@@ -17,19 +18,6 @@ function part1(input) {
     return [...invalidIds].reduce((a,b) => a+b, 0);
 }
 
-function buildNumber(pattern) {
-    return Number(pattern.concat(pattern));
-}
-
-function getPattern(num) {
-    let s = String(num)
-    if (s.length % 2 !== 0) {
-        return '';
-    } else {
-        return s.substring(0,s.length / 2);
-    }
-}
-
 function part2(input) {
     let invalidIds = new Set();
     for ([start, end] of input) {
@@ -37,8 +25,7 @@ function part2(input) {
             let sNum = String(i);
             for (let j = 1; j <= sNum.length / 2; j++) {
                 let pattern = sNum.substring(0,j);
-                if (sNum.length % pattern.length === 0) {
-
+                if (sNum.length % pattern.length === 0) { // only worry about evenly repeatable patterns
                     let patternNum = Number(pattern.repeat(sNum.length / pattern.length));
                     if (!invalidIds.has(patternNum)) {
                         if (patternNum >= start && patternNum <= end) {
@@ -52,5 +39,14 @@ function part2(input) {
     return [...invalidIds].reduce((a,b) => a+b, 0);
 }
 
-// console.log("Part 1 - " + part1(input));
+function getPattern(num) {
+    let s = String(num)
+    if (s.length % 2 !== 0) {
+        return '';
+    } else {
+        return s.substring(0,s.length / 2);
+    }
+}
+
+console.log("Part 1 - " + part1(input));
 console.log("Part 2 - " + part2(input));
