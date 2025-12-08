1b02093d3d30b52cb07c8979fe0efd0cfc10bcaa
Hamilton Evans <glenn.h.evans@gmail.com>
2025-12-08 07:38:07 -0500

    Day 6 Pt 1 - WIP

---
+++ 2025/06/code.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+const {input, testInput} = require('./input');
+const helper = require('../../helpers')
+
+function part1(input) {
+    console.log(input)
+    console.log(_.zip(...input))
+
+    for (let i = 0; i < input[0].length; i++) {
+        
+    }
+    return "tbd";
+}
+
+function part2(input) {
+    return "tbd";
+}
+
+console.log("Part 1 - " + part1(testInput));
+// console.log("Part 2 - " + part2(testInput));
