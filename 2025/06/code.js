7b05fcf461944b3adbac1a889b3104f6418e9777
Hamilton Evans <glenn.h.evans@gmail.com>
2025-12-09 07:34:13 -0500

    Day 6 Pt 1 - Good fun

--- 2025/06/code.js
+++ 2025/06/code.js
@@ -3,18 +3,28 @@ const {input, testInput} = require('./input');
 const helper = require('../../helpers')
 
 function part1(input) {
-    console.log(input)
-    console.log(_.zip(...input))
-
-    for (let i = 0; i < input[0].length; i++) {
-        
+    let eqs = _.zip(...input);
+    let solutions = [];
+    
+    for (eq of eqs) {
+        let func = eq.pop();
+        let vals = eq.map(Number);
+        switch (func) {
+            case '+':
+                solutions.push(vals.reduce((a,b) => a + b, 0));
+                break;
+            case '*':
+                solutions.push(vals.reduce((a,b) => a * b, 1));
+                break;
+        }
     }
-    return "tbd";
+    
+    return solutions.reduce((a,b) => a+b, 0);
 }
 
 function part2(input) {
     return "tbd";
 }
 
-console.log("Part 1 - " + part1(testInput));
+console.log("Part 1 - " + part1(input));
 // console.log("Part 2 - " + part2(testInput));
